80e1b5248113171eb970502c07c791a6e8cb39fe
kretzsch <thomaskretzschmar5@gmail.com>
2023-04-20 21:27:16 +0200

    vital comments and tweaked animation.

--- Assets/Scripts/Audio/LevelAudioController.cs
+++ Assets/Scripts/Audio/LevelAudioController.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
 using FMOD.Studio;
 
+/// <summary>
+///LevelAudioController is responsible for playing the main FMOD event and
+///updating the event's parameters when all enemies in the level are dead.
+/// </summary>
 public class LevelAudioController : MonoBehaviour
 {
+    // EventInstance to control the main FMOD event
     private EventInstance _instance;
     [SerializeField] private LevelManager levelManager;
 
     private void Awake()
     {
+        // Subscribe to the OnAllEnemiesDeadEvent from the StoreChildren component
         StoreChildren storeChildren = FindObjectOfType<StoreChildren>();
         if (storeChildren != null)
         {
@@ -17,12 +23,14 @@ public class LevelAudioController : MonoBehaviour
 
     private void Start()
     {
+        // Create and play the main FMOD event instance
         _instance = FMODAudioManager.Instance.CreateEventInstance("event:/FmodEvents/MainEvent");
         FMODAudioManager.Instance.PlayEvent(_instance);
     }
 
     private void OnDestroy()
     {
+        // Unsubscribe from the OnAllEnemiesDeadEvent when the object is destroyed
         StoreChildren storeChildren = FindObjectOfType<StoreChildren>();
         if (storeChildren != null)
         {
@@ -30,12 +38,14 @@ public class LevelAudioController : MonoBehaviour
         }
     }
 
+    // Called when all enemies in the level are dead
     public void OnAllEnemiesDead()
     {
         // Assuming you have a method in LevelManager to switch to the next level
         levelManager.SwitchToNextLevel();
     }
 
+    // Set the FMOD parameter value using a parameter name and a label
     public void SetFmodParameter(string parameter, string label)
     {
         _instance.setParameterByNameWithLabel(parameter, label);
--- Assets/Scripts/IDamageable.cs
+++ Assets/Scripts/IDamageable.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-
 public interface IDamageable
 {
     void TakeDamage(float damage);
