4b51d63e7cb00e8172068ca10b5112dec903a3dd
kretzsch <thomaskretzschmar5@gmail.com>
2023-06-12 12:12:49 +0200

    added damageableobject 2D & 3D subclasses

---
+++ Assets/Scripts/Enemies/DamageableObject2D.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageableObject2D : DamageableObject
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        HandleCollision(collision.gameObject);
+    }
+
+    public override void HandleCollision(GameObject other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerStats player = other.GetComponent<PlayerStats>();
+            player.TakeDamage(10); // example value
+        }
+    }
+}
+
